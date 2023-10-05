2cd4a7f2056cd607785f13248b7a447ce0fbb5bd
NeilRow56 <neilrowland56@gmail.com>
2023-10-05 07:08:18 +0100

    Added event change functionality to allow profile image to be changed

--- src/components/forms/AccountProfile.tsx
+++ src/components/forms/AccountProfile.tsx
@@ -1,8 +1,10 @@
 'use client'
 
+import Image from 'next/image'
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
+import React, { useState, ChangeEvent } from 'react'
 
 import {
   Form,
@@ -15,11 +17,8 @@ import {
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { Textarea } from '@/components/ui/textarea'
-import React from 'react'
 
 import { UserValidation } from '@/lib/validations/user'
-import Image from 'next/image'
-import { ChangeEvent } from 'react'
 
 interface Props {
   user: {
@@ -33,15 +32,9 @@ interface Props {
   btnTitle: string
 }
 
-const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
-  e.preventDefault()
-}
-
-function onSubmit(values: z.infer<typeof UserValidation>) {
-  console.log(values)
-}
-
 const AccountProfile = ({ user, btnTitle }: Props) => {
+  const [files, setFiles] = useState<File[]>([])
+
   const form = useForm<z.infer<typeof UserValidation>>({
     resolver: zodResolver(UserValidation),
     defaultValues: {
@@ -51,6 +44,34 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
       bio: user?.bio ? user.bio : '',
     },
   })
+
+  const handleImage = (
+    e: ChangeEvent<HTMLInputElement>,
+    fieldChange: (value: string) => void
+  ) => {
+    e.preventDefault()
+
+    const fileReader = new FileReader()
+
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0]
+      setFiles(Array.from(e.target.files))
+
+      if (!file.type.includes('image')) return
+
+      fileReader.onload = async (event) => {
+        const imageDataUrl = event.target?.result?.toString() || ''
+        fieldChange(imageDataUrl)
+      }
+
+      fileReader.readAsDataURL(file)
+    }
+  }
+
+  function onSubmit(values: z.infer<typeof UserValidation>) {
+    console.log(values)
+  }
+
   return (
     <Form {...form}>
       <form
